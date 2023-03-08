01cb9b65fa44d8939a266749b8429cff0e80a9ac
Mr.Hope <mister-hope@outlook.com>
2023-03-08 18:08:08 +0800

    chore: stash

--- packages/client/src/utils/userAgent.ts
+++ packages/client/src/utils/userAgent.ts
@@ -1,31 +1,28 @@
 /// <reference types="user-agent-data-types" />
 
-export const userAgent = async () => {
-  if (!navigator) {
-    return '';
+export const userAgent = async (): Promise<string> => {
+  if (typeof navigator === 'object') {
+    const { userAgentData } = navigator;
+    let { userAgent } = navigator;
+
+    // https://learn.microsoft.com/en-us/microsoft-edge/web-platform/how-to-detect-win11
+    if (!userAgentData || userAgentData.platform !== 'Windows') {
+      return userAgent;
+    }
+
+    const { platformVersion } = await userAgentData.getHighEntropyValues([
+      'platformVersion',
+    ]);
+
+    if (!platformVersion) return userAgent;
+
+    const isWindows11Later = parseInt(platformVersion.split('.')[0]) >= 13;
+
+    if (isWindows11Later)
+      return userAgent.replace('Windows NT 10.0', 'Windows NT 11.0');
+
+    return userAgent;
   }
 
-  const { userAgentData } = navigator;
-  let ua = navigator.userAgent;
-
-  // https://learn.microsoft.com/en-us/microsoft-edge/web-platform/how-to-detect-win11
-  if (!userAgentData || userAgentData.platform !== 'Windows') {
-    return ua;
-  }
-
-  const { platformVersion } = await userAgentData.getHighEntropyValues([
-    'platformVersion',
-  ]);
-
-  if (!platformVersion) {
-    return ua;
-  }
-
-  const isWindows11Later = parseInt(platformVersion.split('.')[0]) >= 13;
-
-  if (isWindows11Later) {
-    ua = ua.replace('Windows NT 10.0', 'Windows NT 11.0');
-  }
-
-  return ua;
+  return '';
 };
